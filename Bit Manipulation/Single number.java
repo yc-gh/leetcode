1579ee671ed4dace3b9a54a97136941cb223cb36
YC <yashdipsite@gmail.com>
2020-02-08 13:25:38 +0530

    Added files

---
+++ Bit Manipulation/Single number.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/single-number/submissions/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i=0; i<nums.length; i++)
+        {
+            xor ^= nums[i];
+        }
+        
+        return xor;
+    }
+}
