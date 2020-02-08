9b0ce2bebfd736881bf0ac46ad2ffb14098b844b
YC <yashdipsite@gmail.com>
2020-02-08 18:17:41 +0530

    Added files

---
+++ Arrays/Majority Element.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/majority-element/submissions/
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        Integer currmax = null;
+        int count = 0;
+        for(int num : nums)
+        {
+            if(count==0)
+            {
+                currmax=num;
+            }
+            
+            count += (num==currmax) ? 1 : -1;
+            
+        }
+        return currmax;
+    }
+}
