add73b69c884278f742299799d00881783148c05
YC <yashdipsite@gmail.com>
2020-02-11 12:02:59 +0530

    Added files

--- Arrays/Maximum subarray sum.java
+++ Arrays/Maximum subarray sum.java
@@ -14,3 +14,26 @@ class Solution {
         return maxsum;
     }
 }
+
+// Equivalent
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxsum = Integer.MIN_VALUE, currsum = 0;
+        
+        for(int i=0; i<nums.length; i++)
+        {            
+            currsum += nums[i];
+            if(currsum > maxsum)
+            {
+                maxsum = currsum;
+            }
+            if(currsum < 0)
+            {
+                currsum = 0;
+            }
+        }
+        
+        return maxsum;
+    }
+}
