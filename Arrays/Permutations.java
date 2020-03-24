27bd5ae97f5cb3defef27abfc000ee12a6076641
yc-gh <ycpcmr@gmail.com>
2020-03-24 21:20:40 +0530

    Added files

---
+++ Arrays/Permutations.java
@@ -0,0 +1,68 @@
+// https://leetcode.com/problems/permutations/
+
+// Backtracking efficient
+class Solution {
+    public List<List<Integer>> permute(int[] nums) 
+    {
+        List<List<Integer>> list = new ArrayList<>();
+        backtrack(list, new ArrayList<>(), new boolean[nums.length], nums);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, boolean[] used, int[] nums)
+    {
+        if(tempList.size() == nums.length)
+        {
+            list.add(new ArrayList<>(tempList));
+        } 
+        else
+        {
+            for(int i = 0; i < nums.length; i++)
+            { 
+                if(used[i]) continue;
+                
+                tempList.add(nums[i]);
+                used[i] = true;
+                
+                backtrack(list, tempList, used, nums);
+                
+                used[i] = false;
+                tempList.remove(tempList.size()-1);
+            }
+        }
+    }
+}
+
+
+//Backtracking inefficient
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+        getPermutations(nums, 0, nums.length-1, output);
+        return output;
+    }
+    
+    static void getPermutations(int[] nums, int l, int r, List<List<Integer>> output)
+    {
+        if(l==r)
+        {
+            output.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+        }
+        else
+        {
+            for(int i=l; i<=r; i++)
+            {
+                swap(nums, i, l);
+                getPermutations(nums, l+1, r, output);
+                swap(nums, i, l);
+            }
+        }
+    }
+    
+    static void swap(int[] nums, int i, int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
