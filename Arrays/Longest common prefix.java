aeba777e74790418bff724427f41f98e64c0d5d6
yc-gh <ycpcmr@gmail.com>
2020-03-27 18:19:25 +0530

    Added files.java

---
+++ Arrays/Longest common prefix.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/longest-common-prefix/
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs==null || strs.length==0) return "";
+        String pre = strs[0];
+        for(int i=1; i<strs.length; i++)
+        {
+            while(!strs[i].startsWith(pre))
+            {
+                pre = pre.substring(0,pre.length()-1);
+            }
+        }
+        return pre;
+    }
+}
