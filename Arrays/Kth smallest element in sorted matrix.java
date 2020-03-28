b5ca9f897cddce005bfe9038182fc4c6f7037093
yc-gh <ycpcmr@gmail.com>
2020-03-28 20:50:39 +0530

    Added files

--- Arrays/Kth smallest element in sorted matrix.java
+++ Arrays/Kth smallest element in sorted matrix.java
@@ -1,6 +1,24 @@
 // https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
 
-// Binary search TODO
+// Binary search 
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int low=matrix[0][0], high=matrix[matrix.length-1][matrix[0].length-1]+1;
+        while(low<high)
+        {
+            int mid = low + (high-low)/2;
+            int count = 0, j=matrix.length-1;
+            for(int i=0; i<matrix.length; i++)
+            {
+                while(j>=0 && matrix[i][j]>mid) j--;
+                count += (j+1);
+            }
+            if(count<k) low = mid+1;
+            else high = mid;
+        }
+        return low;
+    }
+}
 
 // Heap 
 class Solution {
