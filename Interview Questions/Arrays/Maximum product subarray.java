865f5248c5f7f73201018d6f2a50f3b5bc1f50e4
yc-gh <ycpcmr@gmail.com>
2020-04-16 12:08:34 +0530

    Added files

---
+++ Interview Questions/Arrays/Maximum product subarray.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/maximum-product-subarray/
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = nums[0];
+        int tempMax = nums[0];
+        int tempMin = nums[0];
+        
+        for(int i=1; i<nums.length; i++)
+        {
+            if(nums[i]<0)
+            {
+                tempMax = tempMax ^ tempMin;
+                tempMin = tempMin ^ tempMax;
+                tempMax = tempMax ^ tempMin;
+            }
+            
+            tempMax = Integer.max(nums[i], tempMax*nums[i]);
+            tempMin = Integer.min(nums[i], tempMin*nums[i]);
+            
+            if(tempMax>max) max=tempMax;
+        }
+        
+        return max;
+    }
+}
