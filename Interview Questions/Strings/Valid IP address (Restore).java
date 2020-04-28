78a8a9883298330c03a6bbb9081147de135277f4
yc-gh <ycpcmr@gmail.com>
2020-04-28 16:17:05 +0530

    Added files

--- Interview Questions/Strings/Valid IP address (Restore).java
+++ Interview Questions/Strings/Valid IP address (Restore).java
@@ -6,28 +6,27 @@ public class Solution {
         backtrack(s, 0, 0, res, new StringBuilder());
         return res;
     }
-    
-    private void backtrack(String s, int start, int level, 
-                          ArrayList<String> res, StringBuilder sb)
-    {
-        if(start>s.length() || level>4) return;
-        else if(start==s.length() && level==4)
-        {
+
+    private void backtrack(String s, int start, int level, ArrayList<String> res, StringBuilder sb) {
+        if (start > s.length() || level > 4)
+            return;
+        else if (start == s.length() && level == 4) {
             res.add(sb.toString());
-            return ;
+            return;
         }
-        for(int i=1; i<=3; i++)
-        {
-            if(start+i > s.length()) break;
-            String temp = s.substring(start,start+i);
+        for (int i = 1; i <= 3; i++) {
+            if (start + i > s.length())
+                break;
+            String temp = s.substring(start, start + i);
             int num = Integer.parseInt(temp);
-            if((i==1) || (i==2 && num>=10 && num<=99) || (i==3 && num>=100 && num<=255))
-            {
+            if ((i == 1) || (i == 2 && num >= 10 && num <= 99) || (i == 3 && num >= 100 && num <= 255)) {
                 sb.append(temp);
-                if(level<3) sb.append(".");
-                backtrack(s, start+i, level+1, res, sb);
-                if(level<3) sb.setLength(sb.length()-1);
-                sb.setLength(sb.length()-temp.length());
+                if (level < 3)
+                    sb.append(".");
+                backtrack(s, start + i, level + 1, res, sb);
+                if (level < 3)
+                    sb.setLength(sb.length() - 1);
+                sb.setLength(sb.length() - temp.length());
             }
         }
     }
