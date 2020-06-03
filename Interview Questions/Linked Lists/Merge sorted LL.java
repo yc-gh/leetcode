da91b9a7710d4664b3ca827e4abecffe33fb7c79
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-06-03 19:58:18 +0530

    Update Merge sorted LL.java

--- Interview Questions/Linked Lists/Merge sorted LL.java
+++ Interview Questions/Linked Lists/Merge sorted LL.java
@@ -1,58 +1,27 @@
-// https://leetcode.com/problems/merge-two-sorted-lists/
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1==null) return l2;
-        if(l2==null) return l1;
-        ListNode newhead = null;
-        ListNode curr1 = l1;
-        ListNode curr2 = l2;
-        if(curr1.val < curr2.val)
-        {
-            newhead = curr1;
-            curr1 = curr1.next;
-        }
-        else
-        {
-            newhead = curr2;
-            curr2 = curr2.next;
-        }
-        ListNode newcurr = newhead;
+        ListNode head = new ListNode(0);
+        ListNode prev = head;
+        ListNode curr1=l1, curr2=l2;
         while(curr1!=null && curr2!=null)
         {
-            if(curr1.val < curr2.val)
+            if(curr1.val<curr2.val)
             {
-                newcurr.next = curr1;
-                newcurr = curr1;
+                prev.next = curr1;
+                prev = curr1;
                 curr1 = curr1.next;
             }
             else
             {
-                newcurr.next = curr2;
-                newcurr = curr2;
+                prev.next = curr2;
+                prev = curr2;
                 curr2 = curr2.next;
             }
         }
         
-        if(curr1!=null)
-        {
-            newcurr.next = curr1;
-            newcurr = curr1;
-        }
-        if(curr2!=null)
-        {
-            newcurr.next = curr2;
-            newcurr = curr2;
-        }
+        if(curr1!=null) prev.next=curr1;
+        else if(curr2!=null) prev.next=curr2;
         
-        return newhead;
+        return head.next;
     }
 }
