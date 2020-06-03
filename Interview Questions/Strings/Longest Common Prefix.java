03ed0e651e23cec992bb5d5ac3cb4c90e9e8c47a
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-06-02 21:07:18 -0700

    Create Longest Common Prefix.java

---
+++ Interview Questions/Strings/Longest Common Prefix.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/longest-common-prefix/
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0 || strs[0].length()==0) return "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<strs[0].length(); i++)
+        {
+            char ch = strs[0].charAt(i);
+            for(String str : strs)
+            {
+                if(i>=str.length() || str.charAt(i)!=ch) return sb.toString();
+            }
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
