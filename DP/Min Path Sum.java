8ffd1d59c487de59a69522bfa45a4ba52ac69fce
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-07-03 18:10:21 +0530

    Update Min Path Sum.java

--- DP/Min Path Sum.java
+++ DP/Min Path Sum.java
@@ -17,3 +17,27 @@ class Solution {
         return dp[0][0];
     }
 }
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] memo = new int[n+1][m+1];
+        for(int i=0; i<=n; i++) Arrays.fill(memo[i], -1);
+        return dp(grid, 0, 0, memo);
+    }
+    
+    private static int dp(int[][] grid, int x, int y, int[][] memo)
+    {
+        if(x==grid.length-1 && y==grid[0].length-1) 
+        {
+            return memo[x][y] = grid[x][y];
+        }
+        
+        else if(x>=grid.length || y>=grid[0].length) return Integer.MAX_VALUE;
+        
+        if(memo[x][y] != -1) return memo[x][y];
+        
+        return memo[x][y] = grid[x][y] + Integer.min(dp(grid,x+1,y,memo), dp(grid,x,y+1,memo));
+    }
+}
