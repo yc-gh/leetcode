98242e642a728482d73e4c4e498df6cd54f325a0
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-09-11 14:15:31 +0530

    Update Merge two binary trees.java

--- Trees/Merge two binary trees.java
+++ Trees/Merge two binary trees.java
@@ -3,10 +3,11 @@
 class Solution {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
         if(t1==null && t2==null) return null;
-        TreeNode t3 = new TreeNode();
-        t3.val = (t1==null ? 0 : t1.val) + (t2==null ? 0 : t2.val);
-        t3.left = mergeTrees((t1==null ? null : t1.left), (t2==null ? null : t2.left));
-        t3.right = mergeTrees((t1==null ? null : t1.right), (t2==null ? null : t2.right));
+        else if(t1==null) return t2;
+        else if(t2==null) return t1;
+        TreeNode t3 = new TreeNode(t1.val+t2.val);
+        t3.left = mergeTrees(t1.left, t2.left);
+        t3.right = mergeTrees(t1.right, t2.right);
         return t3;
     }
 }
