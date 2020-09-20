fa0371181ee073bb34c6503bd17949cfd6943df9
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-09-20 14:22:32 +0530

    Update Coin change.java

--- Dynamic Programming/Coin change.java
+++ Dynamic Programming/Coin change.java
@@ -1,70 +1,52 @@
-<<<<<<< HEAD:DP/Coin change.java
-// https://leetcode.com/problems/coin-change/
-
-// Bottom up
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (coins == null || coins.length == 0 || amount == 0)
-            return 0;
-        int[] dp = new int[amount + 1];
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            dp[i] = Integer.MAX_VALUE;
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i && dp[i - coins[j]] != Integer.MAX_VALUE) {
-                    dp[i] = Integer.min(dp[i], 1 + dp[i - coins[j]]);
-                }
-            }
-        }
-
-        if (dp[amount] != Integer.MAX_VALUE)
-            return dp[amount];
-
-        return -1;
-    }
-}
-
-// Top down
-=======
-// https://leetcode.com/problems/coin-change/
-
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-        if(amount<1) return 0;
-        return dp(coins, amount, new int[amount+1]);
+        if(coins==null || coins.length==0) return -1;
+        if(amount == 0) return 0;
+        
+        // int[] memo = new int[amount+1];
+        // return topDown(coins, amount, memo);
+        
+        return bottomUp(coins, amount);
     }
     
-    private static int dp(int[] coins, int n, int[] dp)
-    {   
-        if(n<0) return -1;
-        if(n==0) return 0;
-        if(dp[n]!=0) return dp[n];
-        int min = Integer.MAX_VALUE;
-        for(int coin : coins)
-        {
-            if(coin <= n)
-            {
-                int res = dp(coins, n-coin, dp);
-                if(res>=0 && res<min) min = res+1;
-            }
-        }
-        dp[n] = (min==Integer.MAX_VALUE) ? -1 : min;
-        return dp[n];
-    }
-    
-    private static int dp(int[] coins, int n)
+    private int bottomUp(int[] coins, int amount)
     {
-        int m = coins.length;
-        int[] dp = new int[n+1];
-        for(int i=1; i<=n; i++)
+        int[] dp = new int[amount+1];
+        
+        Arrays.fill(dp, Integer.MAX_VALUE-1); // or amount + 1
+        
+        dp[0] = 0;
+        
+        for(int i=1; i<=amount; i++)
         {
-            int min = Integer.MAX_VALUE;
             for(int coin : coins)
             {
-                if(i-coin>=0 && dp[i-coin]!=-1)
-                    min = dp[i]=dp[i-coin] 
+                if(coin <= i) dp[i] = Integer.min(dp[i], dp[i-coin] + 1);
             }
         }
+        
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+    
+    private int topDown(int[] coins, int amount, int[] memo)
+    {
+        if(amount < 0) return -1;
+        else if(amount == 0) return 0;
+        
+        if(memo[amount] != 0) return memo[amount];
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(int coin : coins)
+        {
+            int rem = topDown(coins, amount-coin, memo);
+            if(rem >= 0 && rem < min)
+            {
+                min = 1 + rem;
+            }
+        }
+        
+        memo[amount] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return memo[amount];
     }
 }
->>>>>>> 0ce27c1e613e423652af7f144fdb9f5032f5df8a:Interview Questions/DP/Coin change.java
