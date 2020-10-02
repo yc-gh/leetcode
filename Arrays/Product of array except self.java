93ea1f2b2233fe20b70c89792fee72047e99f8d7
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-10-02 13:28:03 +0530

    Update Product of array except self.java

--- Arrays/Product of array except self.java
+++ Arrays/Product of array except self.java
@@ -2,22 +2,24 @@
 
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-
-        // Left multiplication
-        res[0] = 1;
-        for (int i = 1; i < n; i++) {
-            res[i] = res[i - 1] * nums[i - 1];
+        int[] ans = new int[nums.length];
+        
+        int left = 1;
+        
+        for(int i=0; i<nums.length; i++)
+        {
+            ans[i] = left;
+            left *= nums[i];
         }
-
-        // Right multiplication
+        
         int right = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            res[i] *= right;
+        
+        for(int i=nums.length-1; i>=0; i--)
+        {
+            ans[i] *= right;
             right *= nums[i];
         }
-
-        return res;
+        
+        return ans;
     }
 }
