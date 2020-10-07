f0c6405ffa12e8b3177f490725389bf833531ab5
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-10-07 17:47:03 +0530

    Update Meeting Rooms II.java

--- Greedy/Meeting Rooms II.java
+++ Greedy/Meeting Rooms II.java
@@ -2,17 +2,13 @@
 
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        if(intervals.length==0) return 0;
-        
         Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
         
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        pq.offer(intervals[0][1]);
-        
-        for(int i=1; i<intervals.length; i++)
+        for(int i=0; i<intervals.length; i++)
         {
-            if(pq.peek() <= intervals[i][0]) pq.poll();
+            if(!pq.isEmpty() && pq.peek() <= intervals[i][0]) pq.poll();
             
             pq.offer(intervals[i][1]);
         }
