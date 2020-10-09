3601c2337fa96fa28c7b3b3686a8cdb7c6123b1a
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-10-09 11:45:35 +0530

    Create Reorder data in logs file.java

---
+++ Strings/Reorder data in logs file.java
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/reorder-data-in-log-files/
+
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        if(logs.length < 2) return logs;
+        
+        Arrays.sort(logs, new Comparator<String>(){
+            public int compare(String log1, String log2)
+            {
+                String[] split1 = log1.split(" ", 2);
+                String[] split2 = log2.split(" ", 2);
+
+                boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+                boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+
+                if (!isDigit1 && !isDigit2) {
+                    int cmp = split1[1].compareTo(split2[1]);
+                    if (cmp != 0) return cmp;
+                    return split1[0].compareTo(split2[0]);
+                }
+                
+                if (!isDigit1 && isDigit2)
+                    return -1;
+                else if (isDigit1 && !isDigit2)
+                    return 1;
+                else
+                    return 0;
+            }
+        });
+        
+        return logs;
+    }
+}
