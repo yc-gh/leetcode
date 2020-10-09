8e65ba325460947dafc0d6782209aefeacd36dc1
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-10-09 10:34:39 +0530

    Update Compare version numbers.java

--- Strings/Compare version numbers.java
+++ Strings/Compare version numbers.java
@@ -1,27 +1,18 @@
 // https://www.interviewbit.com/problems/compare-version-numbers/
 
-public class Solution {
-    public int compareVersion(String A, String B) {
-        String[] v1 = A.split("\\.");
-        String[] v2 = B.split("\\.");
-        
-        int size = Math.max(v1.length, v2.length);
-        int i = 0;
-        while (i < size){
-            try{
-            long ver1 = i >= v1.length ? 0 : Long.parseLong(v1[i]);
-            long ver2 = i >= v2.length ? 0 : Long.parseLong(v2[i]);
-            
-            if(ver1 > ver2)
-                return 1;
-            if(ver1 < ver2)
-                return -1;
-            }catch(Exception e){
-                int r = v1[i].compareTo(v2[i]);
-                if(r != 0)
-                  return r > 0 ? 1 : -1;
-            }
-            i++;
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] nums1 = version1.split("\\.");
+        String[] nums2 = version2.split("\\.");
+        int n1 = nums1.length, n2 = nums2.length;
+
+        int i1, i2;
+        for (int i = 0; i < Math.max(n1, n2); ++i) {
+          i1 = i < n1 ? Integer.parseInt(nums1[i]) : 0;
+          i2 = i < n2 ? Integer.parseInt(nums2[i]) : 0;
+          if (i1 != i2) {
+            return i1 > i2 ? 1 : -1;
+          }
         }
         
         return 0;
