9a5768bcdb595a2bb52033599b22fc79d5d1362b
yc-gh <36479105+yc-gh@users.noreply.github.com>
2021-03-21 12:42:24 +0530

    Create Convert BST to Greater Sum Tree.java

---
+++ Trees/Convert BST to Greater Sum Tree.java
@@ -0,0 +1,11 @@
+class Solution {
+    int greater = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root==null) return null;
+        bstToGst(root.right);
+        root.val += greater;
+        greater = root.val;
+        bstToGst(root.left);
+        return root;
+    }
+}
