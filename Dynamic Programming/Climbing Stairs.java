52452996d0901bf8f6fd692081753f7daef24c8a
yc-gh <36479105+yc-gh@users.noreply.github.com>
2021-03-22 08:00:20 +0530

    Update Climbing Stairs.java

--- Dynamic Programming/Climbing Stairs.java
+++ Dynamic Programming/Climbing Stairs.java
@@ -2,18 +2,28 @@
 
 class Solution {
     public int climbStairs(int n) {
-        if(n==0 || n==1) return 1;
-        if(n==2) return 2;
+        // int[] memo = new int[n + 1];
+        // return topDown(0, n, memo);
+        return bottomUp(n);
+    }
+    
+    private int topDown(int step, int total, int[] memo) {
+        if(step == total) return 1;
+        else if(step > total) return 0;
+        if(memo[step] > 0) return memo[step];
+        return memo[step] = topDown(step+1, total, memo) + topDown(step+2, total, memo);
+    }
+    
+    private int bottomUp(int total) {
+        int[] dp = new int[total + 2];
         
-        int[] dp = new int[n+1];
-        dp[0] = dp[1] = 1;
-        dp[2] = 2;
+        dp[total] = 1;
         
-        for(int i=3; i<=n; i++)
+        for(int i=total-1; i>=0; i--) 
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = dp[i+1] + dp[i+2];
         }
         
-        return dp[n];
+        return dp[0];
     }
 }
