283149632fc06546ab89d21b1b001788b64776db
yc-gh <36479105+yc-gh@users.noreply.github.com>
2022-01-05 09:43:26 +0530

    Update Coin change.java

--- Dynamic Programming/Coin change.java
+++ Dynamic Programming/Coin change.java
@@ -2,53 +2,46 @@
 
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(coins==null || coins.length==0) return -1;
-        if(amount == 0) return 0;
-        
-        // int[] memo = new int[amount+1];
+        int[] memo = new int[amount+1];
         // return topDown(coins, amount, memo);
-        
         return bottomUp(coins, amount);
     }
     
-    private int bottomUp(int[] coins, int amount)
-    {
+    private int bottomUp(int[] coins, int amount) {
         int[] dp = new int[amount+1];
         
-        Arrays.fill(dp, Integer.MAX_VALUE-1); // or amount + 1
-        
-        dp[0] = 0;
-        
-        for(int i=1; i<=amount; i++)
-        {
-            for(int coin : coins)
-            {
-                if(coin <= i) dp[i] = Integer.min(dp[i], dp[i-coin] + 1);
+        for(int i=1; i<=amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for(int coin : coins) {
+                if(i >= coin) {
+                    if(dp[i-coin] != Integer.MAX_VALUE) {
+                        dp[i] = Integer.min(dp[i], dp[i-coin] + 1); 
+                    }
+                }
             }
         }
         
-        return dp[amount] > amount ? -1 : dp[amount];
+        return (dp[amount] > amount) ? -1 : dp[amount];
     }
     
-    private int topDown(int[] coins, int amount, int[] memo)
-    {
-        if(amount < 0) return -1;
-        else if(amount == 0) return 0;
+    private int topDown(int[] coins, int amount, int[] memo) {
+        if(amount == 0) return 0;
         
         if(memo[amount] != 0) return memo[amount];
         
+        //Reducing the amount by a large coin could result in more smaller coins required (ex. more 1s)
+        //Instead, reducing by a smaller coin could result in lesser number of total coins
         int min = Integer.MAX_VALUE;
         
-        for(int coin : coins)
-        {
-            int rem = topDown(coins, amount-coin, memo);
-            if(rem >= 0 && rem < min)
-            {
-                min = 1 + rem;
+        for(int i=coins.length-1; i>=0; i--) {
+            if(amount >= coins[i]) {
+                int res = topDown(coins, amount - coins[i], memo);
+                if(res != -1) {
+                    min = Integer.min(min, res);
+                }
             }
         }
-        
-        memo[amount] = (min == Integer.MAX_VALUE) ? -1 : min;
-        return memo[amount];
+                
+        return memo[amount] = (min == Integer.MAX_VALUE ? -1 : min + 1);
     }
 }
