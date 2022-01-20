400968c1de7e1f8bf3e9f1e4ebd1377e59ba5a15
yc-gh <36479105+yc-gh@users.noreply.github.com>
2022-01-20 10:08:05 +0530

    Create Find the duplicate in an array of N+1 integers.java

---
+++ Prep/Find the duplicate in an array of N+1 integers.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int fast = nums[0], slow = nums[0];
+        
+        do
+        {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        } while(fast!=slow);
+
+        slow = nums[0];
+        while(fast!=slow)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return fast;
+    }
+}
