0962547f06d5705a8919b26b2baa7b32b34fbafa
yc-gh <36479105+yc-gh@users.noreply.github.com>
2022-01-25 11:23:38 +0530

    Create Find intersection point of Y LinkedList.java

---
+++ Prep/Linked List - 1/Find intersection point of Y LinkedList.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode currA = headA, currB = headB;
+        
+        while(currA != currB) {
+            currA = currA == null ? headB : currA.next;
+            currB = currB == null ? headA : currB.next;
+        }
+        
+        return currA;
+    }
+}
